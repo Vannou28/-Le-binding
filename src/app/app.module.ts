fc5117710f89dab5e478924a0289cd4202a4a201
Vannou28 <vannier.aurelien2@gmail.com>
2022-11-30 22:38:22 +0100

    04 - Router : creation de route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { DisplayChangeColorDirective } from './display-change-color.directive';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import {ROUTES} from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
-    DisplayChangeColorDirective
+    DisplayChangeColorDirective,
+    MenuComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const ROUTES: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+];
+
+export { ROUTES };
